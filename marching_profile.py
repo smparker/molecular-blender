06c4a6eb365ee9480eabca2155ccf80e9321a4ca
Shane Parker <shane.parker@case.edu>
2025-03-01 21:27:31 -0500

    profile update

--- marching_profile.py
+++ marching_profile.py
@@ -13,12 +13,20 @@ import cProfile
 
 import molecular_blender as mb
 
-def run():
+
+def orbital_isosurface():
     geo = mb.importers.molecule_from_file('examples/zine.molden', {})
     orbitals = mb.orbitals.MOData.from_dict(geo)
-    xyz = np.linspace(-10, 10, 50)
 
     mb.isosurfaces.molden_isosurface(orbitals.get_orbital(12), [0.01, -0.01], 0.01)
 
+def density_isosurface():
+    geo = mb.importers.molecule_from_file('examples/zine.molden', {})
+    orbitals = mb.orbitals.MOData.from_dict(geo)
+
+    mb.isosurfaces.molden_isosurface(orbitals.get_density("density"), [0.01], 0.01)
+
+def run():
+    density_isosurface()
+
 cProfile.run('run()', sort="time")
-#run()
